a5dab695f033a3a4e20f56fa3e7663783188b629
Ronnie Isurina <ronnie.isurina@gmail.com>
2017-05-04 23:03:01 +0800

    Update routes.

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,82 @@
 * Web Routes
 */
 
+/** Settings Routes */
+Route::group(['prefix' => 'school/setting'], function() {
+    Route::get('/', [
+      'uses' => 'SettingController@settingIndex',
+      'as' => 'setting.index',
+    ]);
+});
+/** End Settings Route */
+
+/** Student Payment Route */
+Route::group(['prefix' => 'school/student/payment'], function() {
+    Route::post('/create',[
+      'uses' => 'StudentPaymentController@paymentCreate',
+      'as' => 'studentPayment.create'
+    ]);
+
+    Route::post('/update',[
+      'uses' => 'StudentPaymentController@paymentUpdate',
+      'as' => 'studentPayment.update'
+    ]);
+
+    Route::post('/cancel',[
+      'uses' => 'StudentPaymentController@paymentCancel',
+      'as' => 'studentPayment.cancel'
+    ]);
+
+    Route::post('/restore',[
+      'uses' => 'StudentPaymentController@paymentRestore',
+      'as' => 'studentPayment.restore'
+    ]);
+});
+/** End Student Payment Route */
+
+/** Student Fee Route */
+Route::group(['prefix' => 'school/student/fee'], function() {
+    Route::post('/list', [
+      'uses' => 'StudentFeeController@feeList',
+      'as' => 'studentFee.list'
+    ]);
+
+    Route::post('/create',[
+      'uses' => 'StudentFeeController@feeCreate',
+      'as' => 'studentFee.create'
+    ]);
+
+    Route::post('/update',[
+      'uses' => 'StudentFeeController@feeUpdate',
+      'as' => 'studentFee.update'
+    ]);
+
+    Route::post('/delete',[
+      'uses' => 'StudentFeeController@feeDelete',
+      'as' => 'studentFee.delete'
+    ]);
+});
+/** End Student Fee Route */
+
+/** Student Progress Route */
+Route::group(['prefix' => 'school/student/progress'], function() {
+    Route::post('/enroll', [
+      'uses' => 'StudentProgressController@studentProgressEnroll',
+      'as' => 'studentProgress.enroll'
+    ]);
+
+    Route::post('/update', [
+      'uses' => 'StudentProgressController@studentProgressUpdate',
+      'as' => 'studentProgress.update'
+    ]);
+
+    Route::post('/print', [
+      'uses' => 'StudentProgressController@studentProgressPrint',
+      'as' => 'studentProgress.print'
+    ]);
+});
+/** End Student Progress Route */
+
 /** Student Route */
 Route::group(['prefix' => 'school/student'], function() {
     Route::get('/', [
@@ -30,38 +106,119 @@ Route::group(['prefix' => 'school/student'], function() {
       'uses' => 'StudentController@studentRestore',
       'as' => 'student.restore'
     ]);
-    Route::get('/profile/{id}', [
+    Route::get('/profile/{id}/{sy?}', [
       'uses' => 'StudentController@studentProfile',
       'as' => 'student.profile'
     ]);
 });
 /** End Student Route */
 
+/** School Year Level Route */
+Route::group(['prefix' => 'school/sy/level'], function() {
+    Route::post('/list', [
+      'uses' => 'SchoolYearLevelController@levelList',
+      'as' => 'syLevel.list'
+    ]);
+
+    Route::post('/create', [
+      'uses' => 'SchoolYearLevelController@levelCreate',
+      'as' => 'syLevel.create'
+    ]);
+
+    Route::post('/delete', [
+      'uses' => 'SchoolYearLevelController@levelDelete',
+      'as' => 'syLevel.delete'
+    ]);
+});
+/** End School Year Level Route */
+
+/** School Year Level Fee Route */
+Route::group(['prefix' => 'school/level/fee'], function() {
+    Route::post('/list', [
+      'uses' => 'SchoolYearLevelFeeController@feeList',
+      'as' => 'sylvlfee.list'
+    ]);
+
+    Route::post('/create',[
+      'uses' => 'SchoolYearLevelFeeController@feeCreate',
+      'as' => 'sylvlfee.create'
+    ]);
+
+    Route::post('/update',[
+      'uses' => 'SchoolYearLevelFeeController@feeUpdate',
+      'as' => 'sylvlfee.update'
+    ]);
+
+    Route::post('/delete',[
+      'uses' => 'SchoolYearLevelFeeController@feeDelete',
+      'as' => 'sylvlfee.delete'
+    ]);
+});
+/** End School Year Level Fee Route */
+
 /** Section Route */
 Route::group(['prefix' => 'school/level/section'], function() {
-    Route::post('/create',[
-      'uses' => 'SectionController@secCreate',
-      'as' => 'sec.create'
+    Route::post('/list', [
+      'uses' => 'SchoolYearLevelSectionController@sectionList',
+      'as' => 'section.list'
     ]);
+
+    Route::post('/create',[
+      'uses' => 'SchoolYearLevelSectionController@sectionCreate',
+      'as' => 'section.create'
+    ]);
+
     Route::post('/update',[
-      'uses' => 'SectionController@secUpdate',
-      'as' => 'sec.update'
+      'uses' => 'SchoolYearLevelSectionController@sectionUpdate',
+      'as' => 'section.update'
+    ]);
+
+    Route::post('/delete',[
+      'uses' => 'SchoolYearLevelSectionController@sectionDelete',
+      'as' => 'section.delete'
     ]);
 });
 /** End Section Route */
+
+/** Schedule Route */
+Route::group(['prefix' => 'school/schedule'], function () {
+  Route::post('/table',[
+    'uses' => 'ScheduleController@scheduleTable',
+    'as' => 'schedule.table'
+  ]);
+  Route::post('/create',[
+    'uses' => 'ScheduleController@scheduleCreate',
+    'as' => 'schedule.create'
+  ]);
+  Route::post('/update',[
+    'uses' => 'ScheduleController@scheduleUpdate',
+    'as' => 'schedule.update'
+  ]);
+});
+/** End Schedule Route */
+
+/** Fee Route */
+Route::group(['prefix' => 'school/fee'], function () {
+  Route::post('/table',[
+    'uses' => 'FeeController@feeTable',
+    'as' => 'fee.table'
+  ]);
+  Route::post('/create',[
+    'uses' => 'FeeController@feeCreate',
+    'as' => 'fee.create'
+  ]);
+  Route::post('/update',[
+    'uses' => 'FeeController@feeUpdate',
+    'as' => 'fee.update'
+  ]);
+});
+/** End Fee Route */
+
 /** Level Route */
 Route::group(['prefix' => 'school/level'], function () {
-  Route::get('/', [
-    'uses' => 'LevelController@lvlIndex',
-    'as' => 'lvl.index'
-  ]);
   Route::post('/table',[
     'uses' => 'LevelController@lvlTable',
     'as' => 'lvl.table'
-  ]);
-  Route::get('/{year}', [
-      'uses' => 'LevelController@getLvlTable',
-      'as' => 'lvl.getTable'
   ]);
   Route::post('/create',[
     'uses' => 'LevelController@lvlCreate',
@@ -114,8 +271,13 @@ Route::group(['prefix' => 'school/year'], function() {
     'as' => 'sy.update'
   ]);
   Route::get('/{year}', [
-    'uses' => 'SchoolYearController@syView',
-    'as' => 'sy.view'
+    'uses' => 'SchoolYearController@syProfile',
+    'as' => 'sy.profile'
+  ]);
+  // student list
+  Route::post('/{year}/student/table',[
+    'uses' => 'SchoolYearController@syStudentTable',
+    'as' => 'sy.studentTable'
   ]);
 });
 /** End School Year Route **/
@@ -152,9 +314,12 @@ Route::group(['prefix' => 'school'], function() {
   ]);
 });
 
+/*
 Route::get('/', function () {
     return view('welcome');
 });
+*/
+Route::get('/', [ 'uses' => 'HomeController@index' ] );
 
 Route::get('/home', 'HomeController@index')->name('home');
 
